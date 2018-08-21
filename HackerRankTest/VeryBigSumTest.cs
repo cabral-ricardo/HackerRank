32e90ba7fc740a84d8add32d55548b71d0a2e69c
Ricardo Cabral <ricardocabral.net@gmail.com>
2018-08-21 10:01:16 -0300

    create unit tests sum array

---
+++ HackerRankTest/VeryBigSumTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using HackerRank;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankTest
+{
+    [TestClass]
+    public class VeryBigSumTest
+    {
+        [TestMethod]
+        public void SumArray4()
+        {
+            long[] ar = {1, 3};
+
+            Assert.AreEqual("4", SumArray(ar));
+        }
+
+        [TestMethod]
+        public void SumArray5000000020()
+        {
+            long[] ar = { 5, 1000000001, 1000000002, 1000000003, 1000000004, 1000000005 };
+
+            Assert.AreEqual("5000000020", SumArray(ar));
+        }
+
+        [TestMethod]
+        public void SumArray10047338136()
+        {
+            long[] ar = { 10, 1001458909, 1004570889, 1007019111, 1003302837, 1002514638, 1006431461, 1002575010, 1007514041, 1007548981, 1004402249 };
+
+            Assert.AreEqual("10047338136", SumArray(ar));
+        }
+
+        private static string SumArray(long[] ar)
+        {
+            return VeryBigSum.GetVeryBigSum(ar).ToString();
+        }
+    }
+}
---
+++ VeryBigSum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    public static class VeryBigSum
+    {
+        public static long GetVeryBigSum(long[] ar)
+        {
+            return ar.Sum(); 
+        }
+    }
+}
